08935b320ae18a132c7b1fdb1772e7d1028caeca
gsElen4 <elenagonzalezboiro@gmail.com>
2025-10-10 13:36:02 +0200

    10-10-2025

--- Ficheros/Ficheros/src/Ejercicio5.java
+++ Ficheros/Ficheros/src/Ejercicio5.java
@@ -1,5 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio5 {
+    public static int[] analizarFichero(String rutaFichero) {
+        File archivo  = new File(rutaFichero);
+        if(!archivo.exists()){
+            return null;
+        }
+        int numCaracteres = 0;
+        int numLineas = 0;
+        int numPalabras = 0;
+
+        try(BufferedReader br = new BufferedReader(new FileReader(archivo))){
+            String linea;
+            while((linea =br.readLine()) != null){
+                numLineas++;
+                numCaracteres += linea.length();
+
+                String [] palabras = linea.trim().split("\\s+");
+                if (!linea.trim().isEmpty()){
+                    numPalabras += palabras.length;
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Error al leer el fichero: " + e.getMessage());
+        }
+        return  new int[] {
+            numCaracteres, numLineas, numPalabras
+        };
+     
+    }
     public static void main(String[] args) {
+        String ruta = "D:\\ElenaGonzalez\\AD\\Ficheros\\archivos\\fich02.txt";
         
+        int[]resultado = analizarFichero(ruta);
+
+        if(resultado == null){
+            System.out.println("El fichero no existe");
+        }else{
+            System.out.println("Número de caracteres: " + resultado[0]);
+            System.out.println("Número de lineas: " + resultado[1]);
+            System.out.println("Número de palabras: " + resultado[2]);
+        }
     }
 }
---
+++ Ficheros/Ficheros/src/Ejercicio6.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) throws Exception{
+    try(BufferedWriter bw = Files.newBufferedWriter(Path.of("D:\\ElenaGonzalez\\AD\\Ficheros\\alumnos.csv"),StandardCharsets.UTF_8)){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Introduce nombre del alumno: ");
+            String nombre = sc.nextLine();
+            if (nombre.equals("Z")) {
+                break;
+            }
+            bw.write(nombre);
+            bw.write(";");
+
+            System.out.println("Introduce la fecha: ");
+            String fecha = sc.nextLine();
+            bw.write(fecha);
+            for(int i = 1; i<=3;i++){
+                bw.write(";");
+                System.out.println("Introduce la nota de la " + i + "ª evaluación: ");
+                String nota = sc.nextLine();
+                bw.write(nota);
+            }
+            bw.newLine();
+        }
+        } catch (Exception e){
+
+        }
+
+    }
+}
+
