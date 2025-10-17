2c0a9f2faa971139c8c95c45625b487e8e891c10
gsElen4 <elenagonzalezboiro@gmail.com>
2025-10-17 12:32:40 +0200

    17-10-2025

---
+++ Practica/Agenda/src/Agenda.java
@@ -0,0 +1,6 @@
+public class Agenda {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+ 
---
+++ Practica/Agenda/src/Contacto.java
@@ -0,0 +1,40 @@
+public class Contacto {
+    private String nombre;
+    private double salario;
+    private String puesto;
+    private int edad;
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+     public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+     public String getPuesto(){
+        return puesto;
+    }
+
+    public void setPuesto(String puesto){
+        this.puesto = puesto;
+    }
+
+     public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+
+}
