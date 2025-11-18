46a038daed40103de0b7c588e900528eeacae881
gsElen4 <elenagonzalezboiro@gmail.com>
2025-11-18 09:46:13 +0100

    18/11/2025

--- MySQL/Conexion_bd/Conexion_BD/src/Ej2Main.java
+++ MySQL/Conexion_bd/Conexion_BD/src/Ej2Main.java
@@ -1,6 +1,39 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ej2Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
         
+        Ejercicio2 ej2 = new Ejercicio2();
+        Menu menu = new Menu();
+        int num = 0;
+        do { 
+            num = menu.DameOpcion();
+            if(num>0 && num<5){
+                switch (num) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                    break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                    break;
+                    default:
+                        menu.Menu();
+                        break;
+                }
+            } else {
+                System.out.println("Opción no válida");
+            }
+        } while (true);
     }
 
 }
+ 
--- MySQL/Conexion_bd/Conexion_BD/src/Menu.java
+++ MySQL/Conexion_bd/Conexion_BD/src/Menu.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Menu {
 //Facer un programa de gestión de actores. Que permita:
 //-Buscar actores
@@ -5,6 +8,8 @@ public class Menu {
 //-Actualizar actores por id
 //-Insertar novos actores
 //Usa Statement y PreparedStatement cando corresponda
+
+Scanner sc = new Scanner(System.in);
 public void Menu(){
     System.out.println("M E N U ");
     System.out.println("===========");
@@ -13,5 +18,13 @@ public void Menu(){
     System.out.println("3. Actualizar actores por id");
     System.out.println("4. Insertar novos actores");
     System.out.println("Dame una opción: ");
+
+    
 }
+ public int DameOpcion() {
+        Menu();
+        int op = sc.nextInt();
+        sc.nextLine();
+        return op;
+    }
 }
