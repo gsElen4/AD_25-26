0d76b00d7fb492ebba6efebb53eeab939f57e0fa
gsElen4 <elenagonzalezboiro@gmail.com>
2025-11-21 12:25:51 +0100

    21/11/2025

--- MySQL/Conexion_bd/Conexion_BD/src/Ejercicio2.java
+++ MySQL/Conexion_bd/Conexion_BD/src/Ejercicio2.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Ejercicio2 {
@@ -33,6 +34,7 @@ public void buscaActores() throws SQLException{
          ResultSet rs = pstmt.executeQuery();
 
          while(rs.next()){
+
                 System.out.println("Resultados de la búsqueda:");
             System.out.println(rs.getInt("actor_id") + " " + rs.getString("first_name") + " " + rs.getString("last_name"));
         }
@@ -40,12 +42,13 @@ public void buscaActores() throws SQLException{
 
 public void borraActores() throws SQLException{
         Connection conn = DriverManager.getConnection(url, user, password);
+        System.out.println("Introduce el id del actor al que quieres borrar: ");
+        String id = sc.nextLine();
 
         String query =" DELETE FROM actores WHERE actor_id = ?;";
         PreparedStatement pstmt = conn.prepareStatement(query);
 
-        System.out.println("Introduce el id del actor al que quieres borrar: ");
-        String id = sc.nextLine();
+       
 
         pstmt.setString(1, id);
         ResultSet rs = pstmt.executeQuery();
@@ -57,12 +60,47 @@ public void borraActores() throws SQLException{
 }
 
 public void actualizaActores() throws SQLException{
-        Connection conn = DriverManager.getConnection(url, user, password);
+        try(Connection conn = DriverManager.getConnection(url, user, password); 
+            Statement stmt= conn.createStatement()){
+
+                System.out.println("Dame el nuevo nombre del actor: ");
+                String nombre = sc.nextLine();
+                System.out.println("Dame el id del actor que se quiere actualizar");
+                int id = sc.nextInt();
+                
+            String query = "UPDATE actor SET first_name = ? WHERE actor_id= ?;";
+
+
+           int rows = stmt.executeUpdate(query);
+           System.out.println("Se han modificado "+ rows + " filas");
+           
+          } catch (SQLException e){
+              e.printStackTrace();
+        }
 }
 
 public void insertaActores() throws SQLException{
-        Connection conn = DriverManager.getConnection(url, user, password);
+    try(Connection conn = DriverManager.getConnection(url, user, password)){
 
+        System.out.println("Dame el nombre del actor a añadir: ");
+        String nombre = sc.nextLine();
+        System.out.println("Dame el apellido del actor a añadir: ");
+        String apellido = sc.nextLine();
+        
+        String query = "INSERT INTO actor(first_name, last_name) VALUES(?, ?);";
+        
+        try(PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, nombre);
+            pstmt.setString(2, apellido);
+            
+            int rows = pstmt.executeUpdate();
+           
+            System.out.println("Se han modificado "+ rows + " filas");
+        }
+        
+    } catch (SQLException e){
+        e.printStackTrace();
+    }
 }
 }
 
